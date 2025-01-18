496409f7c14f2c484acf10533756e37a98489004
frozenlib <frozenlib@users.noreply.github.com>
2025-01-18 19:12:01 +0900

    wip

--- src/message/tests.rs
+++ src/message/tests.rs
@@ -55,6 +55,17 @@ fn raw_message_deserialize_error() -> anyhow::Result<()> {
     Ok(())
 }
 
+#[test]
+fn raw_message_deserialize_notification() -> anyhow::Result<()> {
+    let input = r#"{"jsonrpc":"2.0","method":"test_method","params":{"param1":"value1"}}"#;
+    let m = serde_json::from_str::<RawMessage>(input).unwrap();
+    assert_eq!(m.jsonrpc, "2.0");
+    assert_eq!(m.id, None);
+    assert_eq!(m.method, Some("test_method"));
+    assert_eq!(to_value(m.params)?, json!({"param1": "value1"}));
+    Ok(())
+}
+
 fn to_value(x: Option<&RawValue>) -> Result<Value, serde_json::Error> {
     match x {
         Some(v) => Value::deserialize(v),
